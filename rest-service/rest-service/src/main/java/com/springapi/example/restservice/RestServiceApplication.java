4c7642767cfcaceb858d763f4a1e953cf49ef6f2
davidjimenez0 <djimenez0315@gmail.com>
2020-01-19 16:52:52 -0500

    add greeting model

--- rest-service/rest-service/src/main/java/com/springapi/example/restservice/RestServiceApplication.java
+++ rest-service/rest-service/src/main/java/com/springapi/example/restservice/RestServiceApplication.java
@@ -1,5 +1,6 @@
 package com.springapi.example.restservice;
 
+import model.Greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 		System.out.println("test");
+		Greeting a = new Greeting(121,"a");
 	}
-
+	D:\git\rest-api\rest-service\rest-service\src\main\java\com\springapi\example\restservice\RestServiceApplication.java
 }
--- rest-service/rest-service/src/main/java/model/Greeting.java
+++ rest-service/rest-service/src/main/java/model/Greeting.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getContent() {
+        return content;
+    }
+
+
+
 }
